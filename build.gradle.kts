a4b63d5399fd49f87798cc22270a01eae0505169
RH7155 <drahmanhakim71@gmail.com>
2026-07-19 00:45:47 +0700

    Add missing plugins to root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.protobuf) apply false
 }
