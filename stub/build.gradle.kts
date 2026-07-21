306bc65cd1ad8b0de7088b189d62f6e80bbeabad
RH7155 <drahmanhakim71@gmail.com>
2026-07-21 19:53:51 +0700

    Specify compileSdk and minSdk in stub/build.gradle.kts to fix project configuration failure

--- stub/build.gradle.kts
+++ stub/build.gradle.kts
@@ -3,7 +3,11 @@ plugins {
 }
 
 android {
+    compileSdk = 35
     namespace = "stub"
+    defaultConfig {
+        minSdk = 27
+    }
 //    compileOptions {
 //        sourceCompatibility = JavaVersion.VERSION_17
 //        targetCompatibility = JavaVersion.VERSION_17
