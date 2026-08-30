90cfd7ec37b45d585813f0b70bc086ac7b8e21cb
RH7155 <drahmanhakim71@gmail.com>
2026-08-30 20:48:28 +0700

    feat: implement advanced bubble shapes and fix injection

--- app/src/main/java/com/rhdevs/rhpatch/xposed/features/customization/BubbleTickStyles.kt
+++ app/src/main/java/com/rhdevs/rhpatch/xposed/features/customization/BubbleTickStyles.kt
@@ -2,7 +2,6 @@ package com.rhdevs.rhpatch.xposed.features.customization
 
 import android.content.SharedPreferences
 import android.content.res.Resources
-import android.util.TypedValue
 import com.rhdevs.rhpatch.xposed.core.Feature
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
@@ -17,51 +16,31 @@ class BubbleTickStyles(classLoader: ClassLoader, prefs: SharedPreferences) : Fea
         
         if (bubbleStyle == "default") return
         
-        XposedBridge.hookAllMethods(Resources::class.java, "getDrawable", object : XC_MethodHook() {
+        val hook = object : XC_MethodHook() {
             override fun beforeHookedMethod(param: MethodHookParam) {
                 val res = param.thisObject as Resources
                 val id = param.args[0] as Int
                 try {
                     val name = res.getResourceEntryName(id)
-                    if (bubbleStyle == "ios") {
-                        if (name == "balloon_incoming_normal" || name == "balloon_incoming_normal_ext" || name == "msg_in") {
-                            val customId = res.getIdentifier("wae_bubble_ios_in", "drawable", "com.rhdevs.rhpatch")
-                            if (customId != 0) {
-                                param.args[0] = customId
-                            }
-                        } else if (name == "balloon_outgoing_normal" || name == "balloon_outgoing_normal_ext" || name == "msg_out") {
-                            val customId = res.getIdentifier("wae_bubble_ios_out", "drawable", "com.rhdevs.rhpatch")
-                            if (customId != 0) {
-                                param.args[0] = customId
-                            }
+                    val isIncoming = name == "balloon_incoming_normal" || name == "balloon_incoming_normal_ext" || name == "msg_in"
+                    val isOutgoing = name == "balloon_outgoing_normal" || name == "balloon_outgoing_normal_ext" || name == "msg_out"
+                    
+                    if (isIncoming || isOutgoing) {
+                        val suffix = if (isIncoming) "in" else "out"
+                        val drawableName = "wae_bubble_${bubbleStyle}_${suffix}"
+                        val hostPackage = res.getResourcePackageName(id)
+                        
+                        val customId = res.getIdentifier(drawableName, "drawable", hostPackage)
+                        if (customId != 0) {
+                            param.args[0] = customId
                         }
                     }
                 } catch (e: Exception) {}
             }
-        })
-        
-        XposedBridge.hookAllMethods(Resources::class.java, "getDrawableForDensity", object : XC_MethodHook() {
-            override fun beforeHookedMethod(param: MethodHookParam) {
-                val res = param.thisObject as Resources
-                val id = param.args[0] as Int
-                try {
-                    val name = res.getResourceEntryName(id)
-                    if (bubbleStyle == "ios") {
-                        if (name == "balloon_incoming_normal" || name == "balloon_incoming_normal_ext" || name == "msg_in") {
-                            val customId = res.getIdentifier("wae_bubble_ios_in", "drawable", "com.rhdevs.rhpatch")
-                            if (customId != 0) {
-                                param.args[0] = customId
-                            }
-                        } else if (name == "balloon_outgoing_normal" || name == "balloon_outgoing_normal_ext" || name == "msg_out") {
-                            val customId = res.getIdentifier("wae_bubble_ios_out", "drawable", "com.rhdevs.rhpatch")
-                            if (customId != 0) {
-                                param.args[0] = customId
-                            }
-                        }
-                    }
-                } catch (e: Exception) {}
-            }
-        })
+        }
+
+        XposedBridge.hookAllMethods(Resources::class.java, "getDrawable", hook)
+        XposedBridge.hookAllMethods(Resources::class.java, "getDrawableForDensity", hook)
     }
 }
 
