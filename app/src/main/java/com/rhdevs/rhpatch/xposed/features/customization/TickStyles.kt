09a041c69424758d36e1e9cf7f57cdf071f7bc3a
RH7155 <drahmanhakim71@gmail.com>
2026-08-30 23:30:45 +0700

    fix: Update TickStyles to hook Resources.getDrawable instead of ResourcesImpl

--- app/src/main/java/com/rhdevs/rhpatch/xposed/features/customization/TickStyles.kt
+++ app/src/main/java/com/rhdevs/rhpatch/xposed/features/customization/TickStyles.kt
@@ -2,7 +2,9 @@
 
 import android.content.SharedPreferences
 import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.os.Build
+import android.widget.ImageView
 import com.rhdevs.rhpatch.xposed.core.Feature
 import com.rhdevs.rhpatch.xposed.core.FeatureLoader
 import de.robv.android.xposed.XC_MethodHook
@@ -18,44 +20,46 @@ class TickStyles(classLoader: ClassLoader, prefs: SharedPreferences) : Feature(c
         val tickStyle = prefs.getString("pref_tick_style", "default")
         
         if (tickStyle != "default") {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                try {
-                    val resImplClass = XposedHelpers.findClass("android.content.res.ResourcesImpl", null)
-                    XposedBridge.hookAllMethods(resImplClass, "loadDrawable", object : XC_MethodHook() {
-                        override fun beforeHookedMethod(param: MethodHookParam) {
-                            val res = param.args[0] as Resources
-                            val id = param.args[2] as Int
-                            try {
-                                val name = res.getResourceEntryName(id)
+            try {
+                // Hook getDrawable to intercept when WhatsApp requests the tick icons
+                val getDrawableHook = object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        val res = param.thisObject as Resources
+                        val id = param.args[0] as Int
+                        try {
+                            val name = res.getResourceEntryName(id)
+                            var customName: String? = null
+                            
+                            if (name == "msg_status_server" || name == "msg_status_client_read_any") {
+                                customName = "wae_tick_${tickStyle}_server"
+                            } else if (name == "msg_status_client_delivered" || name == "msg_status_client_read_all") {
+                                customName = "wae_tick_${tickStyle}_delivered"
+                            } else if (name == "msg_status_client_read") {
+                                customName = "wae_tick_${tickStyle}_read"
+                            }
+                            
+                            if (customName != null) {
+                                val moduleContext = FeatureLoader.moduleContext
+                                val moduleRes = moduleContext.resources
+                                val customId = moduleRes.getIdentifier(customName, "drawable", moduleContext.packageName)
                                 
-                                var customName: String? = null
-                                if (name == "msg_status_server" || name == "msg_status_client_read_any") {
-                                    customName = "wae_tick_${tickStyle}_server"
-                                } else if (name == "msg_status_client_delivered" || name == "msg_status_client_read_all") {
-                                    customName = "wae_tick_${tickStyle}_delivered"
-                                } else if (name == "msg_status_client_read") {
-                                    customName = "wae_tick_${tickStyle}_read"
-                                }
-                                
-                                if (customName != null) {
-                                    val moduleContext = FeatureLoader.moduleContext
-                                    val moduleRes = moduleContext.resources
-                                    val customId = moduleRes.getIdentifier(customName, "drawable", moduleContext.packageName)
-                                    
-                                    if (customId != 0) {
-                                        @Suppress("DEPRECATION")
-                                        val customDrawable = moduleRes.getDrawable(customId)
-                                        if (customDrawable != null) {
-                                            param.result = customDrawable
-                                        }
+                                if (customId != 0) {
+                                    @Suppress("DEPRECATION")
+                                    val customDrawable = moduleRes.getDrawable(customId)
+                                    if (customDrawable != null) {
+                                        param.result = customDrawable
                                     }
                                 }
-                            } catch (e: Exception) {}
-                        }
-                    })
-                } catch (e: Throwable) {
-                    XposedBridge.log("Failed to hook tick method: " + e.message)
+                            }
+                        } catch (e: Exception) {}
+                    }
                 }
+                
+                XposedHelpers.findAndHookMethod(Resources::class.java, "getDrawable", Int::class.javaPrimitiveType, getDrawableHook)
+                XposedHelpers.findAndHookMethod(Resources::class.java, "getDrawable", Int::class.javaPrimitiveType, Resources.Theme::class.java, getDrawableHook)
+                
+            } catch (e: Throwable) {
+                XposedBridge.log("Failed to hook getDrawable for ticks: " + e.message)
             }
         }
     }
